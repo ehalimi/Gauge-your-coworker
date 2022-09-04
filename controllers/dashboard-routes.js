e7f7406430378c2a109e135495799c380d3c7470
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-04 16:04:15 -0500

    adding comments works, can also add employees now

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,20 +1,30 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Comment } = require('../models');
+const { Employee, User, Comment } = require('../models');
 cons = require('../utils/authentication');
 
-// get all comments for dashboard
 router.get('/', (req, res) => {
-  console.log(req.session);
-  console.log('======================');
-  Comment.findAll({
-    where: {
-      user_id: req.session.user_id
-    },
+  Employee.findAll({
+    attributes: [
+      'id',
+      'employee_name',
+      'work_name',
+      'position'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'Employee_id', 'user_id', 'created_at'],
+   
+      }
+    ]
   })
-    .then(dbCommentData => {
-      const comment = dbCommentData.map(comments => comments.get({ plain: true }));
-      res.render('dashboard', { comment, loggedIn: true });
+    // map data to get content
+    .then(dbEmployeeData => {
+      const employees = dbEmployeeData.map(employee => employee.get({ plain: true }));
+// render handlebar dashboard for this data and new data. 
+        res.render("dashboard", { employees, loggedIn: true });
+     
     })
     .catch(err => {
       console.log(err);
@@ -28,24 +38,24 @@ router.get('/', (req, res) => {
 //     });
 //   });
 
-router.get('/edit/:id', (req, res) => {
-  Comment.findByPk(req.params.id)
-    .then(dbCommentData => {
-      if (dbCommentData) {
-        const post = dbCommentData.get({ plain: true });
+// router.get('/edit/:id', (req, res) => {
+//   Comment.findByPk(req.params.id)
+//     .then(dbCommentData => {
+//       if (dbCommentData) {
+//         const post = dbCommentData.get({ plain: true });
 
-        res.render("edit-post", {
-            layout: "dashboard",
-            post
-          });
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });  
+//         res.render("edit-post", {
+//             layout: "dashboard",
+//             post
+//           });
+//         } else {
+//           res.status(404).end();
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).json(err);
+//       });
+//   });  
 
 
 module.exports = router;
