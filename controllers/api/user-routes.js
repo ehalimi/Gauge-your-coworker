cb446a3129879007aff4364704d25b351369a2a7
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-03 19:58:39 -0500

    can post/make a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,5 +43,26 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+//make a new user
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router; 
