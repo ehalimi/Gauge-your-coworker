52eb412c0aa980cf84f986cb683d8ff13714b5ac
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-03 20:02:03 -0500

    user can login

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
-    checkPasword(loginPw) {
+    checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 };
