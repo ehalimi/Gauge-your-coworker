665fdaccb488851cbcf7755258d8349dd73f3404
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-03 19:42:56 -0500

    can post commnet, need to add authentication, made it where user_id isn't associated with it for anonymity

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,3 +10,16 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// post comments via /api/comments,TODO add in authentication
+router.post('/', (req, res) => {
+    Comment.create({
+      employee_id: req.body.employee_id,
+      comment_text: req.body.comment_text
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
