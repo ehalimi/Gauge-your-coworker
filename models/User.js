c565e05616e4621023cd83d8442fbcf19da27a49
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-09-06 20:05:56 -0500

    temporarily removing bcrypt

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    // }
 };
 
 User.init(
@@ -37,17 +37,17 @@ User.init(
         }
     },
     {
-        hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
+        // hooks: {
+        //     // set up beforeCreate lifecycle "hook" functionality
+        //     async beforeCreate(newUserData) {
+        //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        //         return newUserData;
+        //     },
+        //     async beforeUpdate(updatedUserData) {
+        //         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        //         return updatedUserData;
+        //     }
+        // },
         sequelize,
         timestamps: false,
         freezeTableName: true,
